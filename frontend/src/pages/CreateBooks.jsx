d02e509f7543a636623b9d82c8880a70131540dc
Shriya <vatsashriya08@gmail.com>
2025-07-06 23:31:48 +0530

    Update CreateBooks.jsx

--- frontend/src/pages/CreateBooks.jsx
+++ frontend/src/pages/CreateBooks.jsx
@@ -13,8 +13,7 @@ const CreateBooks = () => {
 
   const handleSubmit = (e) => {
     e.preventDefault();
-    await axios.post("https://book-store-project-9zfd.onrender.com/api/books", form);
-      .then(() => navigate('/'))
+    axios.post('https://book-store-project-9zfd.onrender.com/books', form).then(() => navigate('/'))
       .catch((err) => console.error(err));
   };
 
